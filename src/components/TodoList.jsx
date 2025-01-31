e65bff71bb1e0112fc397892a6e210c98a068618
Handsol <imhandsol@gmail.com>
2025-01-31 10:40:22 +0900

    feat : Update 기능 추가

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 const SAMPLE_TODOS = [
-  { id: 1, text: "Buy milk" },
-  { id: 2, text: "Clean the house" },
-  { id: 3, text: "Go for a run" },
-  { id: 4, text: "Finish homework" },
-  { id: 5, text: "Call mom" },
-  { id: 6, text: "Buy groceries" },
-  { id: 7, text: "Walk the dog" },
-  { id: 8, text: "Read a book" },
-  { id: 9, text: "Do laundry" },
-  { id: 10, text: "Write code" },
+  { id: 1, text: "Buy milk", completed: false },
+  { id: 2, text: "Clean the house", completed: false },
+  { id: 3, text: "Go for a run", completed: false },
+  { id: 4, text: "Finish homework", completed: false },
+  { id: 5, text: "Call mom", completed: false },
+  { id: 6, text: "Buy groceries", completed: false },
+  { id: 7, text: "Walk the dog", completed: false },
+  { id: 8, text: "Read a book", completed: false },
+  { id: 9, text: "Do laundry", completed: false },
+  { id: 10, text: "Write code", completed: false },
 ];
 
 const TodoList = () => {
@@ -35,6 +35,23 @@ const TodoList = () => {
     setTodoText(e.target.value);
   };
 
+  const handleToggleCompleted = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        const newTodo = {
+          ...todo,
+          completed: !todo.completed,
+        };
+
+        return newTodo;
+      } else {
+        return todo;
+      }
+    });
+
+    setTodos(updatedTodos);
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -42,8 +59,15 @@ const TodoList = () => {
         <button type="submit">제출하기</button>
       </form>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+        {todos.map(({ id, text, completed }) => (
+          <li key={id}>
+            <p style={{ textDecoration: completed ? "line-through" : "none" }}>
+              {text}
+            </p>
+            <button onClick={() => handleToggleCompleted(id)}>
+              {completed ? "취소하기" : "완료하기"}
+            </button>
+          </li>
         ))}
       </ul>
     </div>
