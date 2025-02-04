ce608d2d89c78a95547af7e8bd00fbcf94f65cc9
Handsol <imhandsol@gmail.com>
2025-02-04 10:48:52 +0900

    feat : TodoContainer 구성 요소 추가 및 TodoList에서 로직 분리

---
+++ src/components/todo/TodoContainer.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+const SAMPLE_TODOS = [
+  { id: 1, text: "Buy milk", completed: false },
+  { id: 2, text: "Clean the house", completed: false },
+  { id: 3, text: "Go for a run", completed: false },
+  { id: 4, text: "Finish homework", completed: false },
+  { id: 5, text: "Call mom", completed: false },
+  { id: 6, text: "Buy groceries", completed: false },
+  { id: 7, text: "Walk the dog", completed: false },
+  { id: 8, text: "Read a book", completed: false },
+  { id: 9, text: "Do laundry", completed: false },
+  { id: 10, text: "Write code", completed: false },
+];
+
+const TodoContainer = () => {
+  const [todos, setTodos] = useState(SAMPLE_TODOS);
+  const [todoText, setTodoText] = useState("");
+
+  const handleSubmit = (e) => {
+    // 새로고침 막아주기
+    e.preventDefault();
+
+    if (!todoText.trim()) {
+      return;
+    }
+
+    setTodos([
+      { id: crypto.randomUUID(), text: todoText, completed: false },
+      ...todos,
+    ]);
+
+    setTodoText("");
+  };
+
+  // input 창의 입력값을 가져오는 함수
+  const handleChangeTodoText = (e) => {
+    setTodoText(e.target.value);
+  };
+
+  const handleToggleCompleted = (id) => {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+
+      return todo;
+    });
+
+    setTodos(updateTodos);
+  };
+
+  const handleDelete = (id) => {
+    setTodos((prev) => {
+      return prev.filter((todo) => {
+        if (todo.id === id) {
+          return false;
+        }
+
+        return true;
+      });
+    });
+  };
+
+  return (
+    <div>
+      <TodoForm
+        handleSubmit={handleSubmit}
+        todoText={todoText}
+        handleChangeTodoText={handleChangeTodoText}
+      />
+
+      <TodoList
+        todos={todos}
+        handleToggleCompleted={handleToggleCompleted}
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
+};
+
+export default TodoContainer;
