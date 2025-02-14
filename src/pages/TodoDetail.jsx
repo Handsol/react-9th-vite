f9d7d82c426514d4881f5a7ab1a66f7055fb8591
Handsol <imhandsol@gmail.com>
2025-02-14 12:11:58 +0900

    feat : TodoDetail 페이지에서 TodoContext를 사용하여 할 일 데이터를 가져오고, 데이터가 없을 경우 메시지 표시 기능 추가

--- src/pages/TodoDetail.jsx
+++ src/pages/TodoDetail.jsx
@@ -1,20 +1,26 @@
 import { Link, useParams } from "react-router";
-import { SAMPLE_TODOS } from "../constants/sample-todos";
 import TodoItem, { ActionButton } from "../components/todo/TodoItem";
 import styled from "styled-components";
+import { useContext } from "react";
+import { TodoContext } from "../../context/TodoContext";
 
 const TodoDetail = () => {
+  const { todos } = useContext(TodoContext);
   const { id } = useParams();
 
-  const targetTodoItem = SAMPLE_TODOS.find((todo) => todo.id === +id);
+  const targetTodoItem = todos.find((todo) => todo.id === +id);
 
   return (
     <DetailPageWrapper>
-      <TodoItem
-        id={targetTodoItem.id}
-        text={targetTodoItem.text}
-        completed={targetTodoItem.completed}
-      />
+      {targetTodoItem ? (
+        <TodoItem
+          id={targetTodoItem.id}
+          text={targetTodoItem.text}
+          completed={targetTodoItem.completed}
+        />
+      ) : (
+        <p>해당하는 데이터를 찾을 수 없습니다.</p>
+      )}
       <BackLink to="/">
         <ActionButton $bgColor="#242424">돌아가기</ActionButton>
       </BackLink>
