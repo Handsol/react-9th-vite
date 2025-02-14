f934bc78a26c4764e9e2b542f0c595a0b397a6b6
Handsol <imhandsol@gmail.com>
2025-02-14 10:43:46 +0900

    feat : TodoProvider 추가 및 샘플 할 일 데이터 상수화, TodoDetail 페이지에서 할 일 상세 정보 표시 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import RootLayout from "./components/layout/RootLayout";
 import Home from "./pages/Home";
 import TodoDetail from "./pages/TodoDetail";
+import TodoProvider from "./components/provider/TodoProvider";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RootLayout />}>
-          <Route index element={<Home />} />
-          <Route path="todos/:id" element={<TodoDetail />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <TodoProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route path="todos/:id" element={<TodoDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </TodoProvider>
   );
 };
 
--- src/pages/TodoDetail.jsx
+++ src/pages/TodoDetail.jsx
@@ -1,9 +1,26 @@
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
+import { SAMPLE_TODOS } from "../constants/sample-todos";
+import TodoItem, { ActionButton } from "../components/todo/TodoItem";
+import styled from "styled-components";
 
 const TodoDetail = () => {
   const { id } = useParams();
 
-  return <div>TodoDetail ID : {id}</div>;
+  const targetTodoItem = SAMPLE_TODOS.find((todo) => todo.id === +id);
+
+  return (
+    <div>
+      <TodoItem
+        id={targetTodoItem.id}
+        text={targetTodoItem.text}
+        completed={targetTodoItem.completed}
+      />
+
+      <ActionButton $bgColor="#242424">
+        <Link to="/">돌아가기</Link>
+      </ActionButton>
+    </div>
+  );
 };
 
 export default TodoDetail;
