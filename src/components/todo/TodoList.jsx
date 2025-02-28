9c7b792f8d3a0c9f383ec405e49fcc1b4c8b9369
Handsol <imhandsol@gmail.com>
2025-02-28 09:52:54 +0900

    refactor: TodoList 컴포넌트 리팩토링 - 커스텀 훅 사용 및 TailwindCSS 적용

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,68 +1,22 @@
-import styled from "styled-components";
+import { useFilterParams } from "../../hooks/useFilterParams";
+import { useTodoQuery } from "../../hooks/useTodoQuery";
 import TodoItem from "./TodoItem";
-import { useQuery } from "@tanstack/react-query";
 
 const TodoList = () => {
-  // const [searchParams] = useSearchParams();
-  // const selectedFilter = searchParams.get("filter");
-
-  const {
-    data: todos,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["todos"],
-    queryFn: getTodos,
-  });
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error fetching todos - {error}</div>;
-  }
-
-  const getFilterTodos = (selectedFilter) => {
-    if (selectedFilter === "completed") {
-      return todos.filter((todo) => todo.completed);
-    }
-
-    if (selectedFilter === "pending") {
-      return todos.filter((todo) => !todo.completed);
-    }
-
-    return todos;
-  };
-
-  const filterTodos = getFilterTodos(selectedFilter);
+  const selectedFilter = useFilterParams();
+  const { data: todos } = useTodoQuery(selectedFilter);
 
   return (
-    <TodoListSection>
-      <TodoListHeader>Tasks</TodoListHeader>
-      <TodoListContent>
+    <section className="flex flex-col gap-4">
+      <h2 className="text-2xl font-bold">Tasks</h2>
+
+      <ul className="flex flex-col gap-4">
         {todos?.map(({ id, text, completed }) => (
-          <TodoItem key={id} text={text} completed={completed} id={id} />
+          <TodoItem key={id} completed={completed} text={text} id={id} />
         ))}
-      </TodoListContent>
-    </TodoListSection>
+      </ul>
+    </section>
   );
 };
-
-const TodoListSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
-
-const TodoListHeader = styled.h2`
-  font-size: 1.5rem;
-  font-weight: bold;
-`;
-const TodoListContent = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
 
 export default TodoList;
