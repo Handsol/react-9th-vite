d5c3336f788f515e719668a7fadb052ab7315ec9
Handsol <imhandsol@gmail.com>
2025-02-28 09:48:44 +0900

    feat: RootLayout 컴포넌트 수정 - 스타일 개선 및 테마 전환 버튼 추가

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,26 +1,19 @@
 import { Outlet } from "react-router";
-import styled from "styled-components";
+import ThemeSwitchButton from "../../ThemeSwitchButton";
 
 // Compositional Layout
-const RootLayout = () => {
+export const RootLayout = () => {
   return (
-    <MainContainer>
-      <MainContent>
-        <Outlet />
-      </MainContent>
-    </MainContainer>
+    <main className="min-h-screen bg-[#f6f5f8] dark:bg-slate-800">
+      <div className="max-w-3xl mx-auto p-4">
+        <div>
+          <Outlet />
+        </div>
+
+        <ThemeSwitchButton />
+      </div>
+    </main>
   );
 };
-
-const MainContainer = styled.main`
-  min-height: 100vh;
-  background-color: #f6f5f8;
-`;
-
-const MainContent = styled.div`
-  max-width: 768px;
-  margin: 0 auto;
-  padding: 2rem;
-`;
 
 export default RootLayout;
