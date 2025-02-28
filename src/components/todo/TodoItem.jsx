500d22d739e4470396e1a36b2fb22613e8ccaa48
Handsol <imhandsol@gmail.com>
2025-02-28 09:52:37 +0900

    refactor: styled-components 제거 및 TailwindCSS 적용

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,24 +1,14 @@
-import styled from "styled-components";
 import { Link, useNavigate } from "react-router";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  useDeleteTodoMutation,
+  useToggleTodoMutation
+} from "../../hooks/useTodoQuery";
 
-const TodoItem = ({ id, text, completed }) => {
+const TodoItem = ({ completed, text, id }) => {
   const navigate = useNavigate();
-  const queryClient = useQueryClient();
 
-  const { mutate: toggleTodoMutate } = useMutation({
-    mutationFn: ({ id, completed }) => toggleTodoCompleted(id, completed),
-    onSettled: () => {
-      return queryClient.invalidateQueries(["todos"]);
-    },
-  });
-
-  const { mutate: deleteTodoMutate } = useMutation({
-    mutationFn: deleteTodo,
-    onSettled: () => {
-      return queryClient.invalidateQueries(["todos"]);
-    },
-  });
+  const { mutate: toggleTodoMutate } = useToggleTodoMutation();
+  const { mutate: deleteTodoMutate } = useDeleteTodoMutation();
 
   const navigateAfterDelete = (id) => {
     deleteTodoMutate(id);
@@ -27,73 +17,33 @@ const TodoItem = ({ id, text, completed }) => {
   };
 
   return (
-    <TodoItemWrapper>
-      <TodoItemLink to={`/todos/${id}`} $completed={completed}>
+    <li className="flex flex-row flex-wrap justify-between items-center gap-4 bg-white p-5 rounded-2xl shadow-md">
+      <Link
+        to={`/todos/${id}`}
+        className={`hover:underline ${completed ? " line-through" : ""}`}
+      >
         {text}
-      </TodoItemLink>
+      </Link>
 
-      <TodoItemActions>
-        <ActionButton
-          onClick={() => toggleTodoMutate(id, completed)}
-          $bgColor={completed ? "#242424" : "#582be6"}
+      <div className="flex flex-row flex-wrap gap-2">
+        <button
+          onClick={() => toggleTodoMutate({ id, completed })}
+          className={`${
+            completed ? "bg-[#242424]" : "bg-[#582be6]"
+          } text-white px-4 py-2 rounded-lg hover:opacity-80 whitespace-nowrap text-center`}
         >
           {completed ? "취소하기" : "완료하기"}
-        </ActionButton>
+        </button>
 
-        <ActionButton
+        <button
           onClick={() => navigateAfterDelete(id)}
-          $bgColor="#e6582b"
+          className="bg-[#ff4033] text-white px-4 py-2 rounded-lg hover:opacity-80 whitespace-nowrap text-center"
         >
           삭제하기
-        </ActionButton>
-      </TodoItemActions>
-    </TodoItemWrapper>
+        </button>
+      </div>
+    </li>
   );
 };
-
-const TodoItemWrapper = styled.li`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 1rem;
-
-  background-color: white;
-  padding: 1.25rem;
-  border-radius: 1rem;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-`;
-
-const TodoItemLink = styled(Link)`
-  text-decoration: ${({ $completed }) =>
-    $completed ? "line-through" : "none"};
-
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-const TodoItemActions = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  gap: 1rem;
-`;
-
-export const ActionButton = styled.button`
-  background-color: ${(props) => props.$bgColor};
-  color: white;
-  border: none;
-  padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
-  cursor: pointer;
-  word-break: keep-all;
-  text-align: center;
-
-  &:hover {
-    opacity: 0.8;
-  }
-`;
 
 export default TodoItem;
