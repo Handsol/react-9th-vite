55bc3bb4a1c64d22a99ff4a994028566da8ff012
Handsol <imhandsol@gmail.com>
2025-02-28 09:53:29 +0900

    refactor: App 컴포넌트 수정 - 페이지 컴포넌트 및 테마 관리 훅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import RootLayout from "./components/layout/RootLayout";
-import Home from "./pages/Home";
-import TodoDetail from "./pages/TodoDetail";
-import QueryProvider from "./components/provider/QueryProvider";
+import { HomePage } from "./pages/HomePage";
+import { TodoDetailPage } from "./pages/TodoDetailPage";
+import { RootLayout } from "./components/todo/layout/RootLayout";
+import QueryProvider from "./components/todo/provider/QueryProvider";
+import { useThemeStore } from "./hooks/useThemeStore";
+import { useEffect } from "react";
 
-const App = () => {
+export const App = () => {
+  const { theme } = useThemeStore();
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    //현재 테마 초기화
+    root.classList.remove("dark", "light");
+
+    //테마 설정
+    root.classList.add(theme);
+  }, [theme]);
+
   return (
     <BrowserRouter>
       <QueryProvider>
         <Routes>
           <Route path="/" element={<RootLayout />}>
-            <Route index element={<Home />} />
-            <Route path="todos/:id" element={<TodoDetail />} />
+            <Route index element={<HomePage />} />
+            <Route path="todos/:id" element={<TodoDetailPage />} />
           </Route>
         </Routes>
       </QueryProvider>
