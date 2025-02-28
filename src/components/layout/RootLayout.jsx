c8db68e139559f28fe20e3354b5103bc57368f3d
Handsol <imhandsol@gmail.com>
2025-02-28 10:00:50 +0900

    refactor: RootLayout에서 ThemeSwitchButton 경로 수정

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import ThemeSwitchButton from "../../ThemeSwitchButton";
+import ThemeSwitchButton from "../ThemeSwitchButton";
 
 // Compositional Layout
 export const RootLayout = () => {
