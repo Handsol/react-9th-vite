e9753f9456f2eb9a3bd2339d2504bd4f51af91d2
Handsol <imhandsol@gmail.com>
2025-03-11 17:20:02 +0900

    feat: 클라이언트 사이드 렌더링 페이지 추가 및 Todo 타입 수정

--- api/todo.api.ts
+++ api/todo.api.ts
@@ -1,3 +1,5 @@
+import { Todo } from "@/types/todo.type";
+
 export const getTodos = async () => {
   const response = await fetch("http://localhost:3000/todos");
   const data: Todo[] = await response.json();
