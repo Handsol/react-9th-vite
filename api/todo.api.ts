1ff97b970f6fd6324523ce2aa83272dbd3798bb4
Handsol <imhandsol@gmail.com>
2025-03-13 23:50:18 +0900

    feat: Todo API에 할 일 생성 기능 추가

--- api/todo.api.ts
+++ api/todo.api.ts
@@ -1,8 +1,23 @@
 import { Todo } from "@/types/todo.type";
 
+const BASE_URL = "http://localhost:3000/todos";
+
 export const getTodos = async () => {
-  const response = await fetch("http://localhost:3000/todos");
+  const response = await fetch(BASE_URL);
   const data: Todo[] = await response.json();
+
+  return data;
+};
+
+export const createTodo = async (text: string) => {
+  const response = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ text: text, completed: false })
+  });
+  const data: Todo = await response.json();
 
   return data;
 };
