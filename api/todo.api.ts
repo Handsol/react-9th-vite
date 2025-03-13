462b7f6a8d6bee4b17af4b06af049ef5cb8e2847
Handsol <imhandsol@gmail.com>
2025-03-13 23:52:48 +0900

    feat: Todo API에 할 일 삭제 기능 추가

--- api/todo.api.ts
+++ api/todo.api.ts
@@ -9,6 +9,7 @@ export const getTodos = async () => {
   return data;
 };
 
+// 할 일 생성 로직
 export const createTodo = async (text: string) => {
   const response = await fetch(BASE_URL, {
     method: "POST",
@@ -16,6 +17,16 @@ export const createTodo = async (text: string) => {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({ text: text, completed: false })
+  });
+  const data: Todo = await response.json();
+
+  return data;
+};
+
+// 할 일 삭제 로직
+export const deleteTodo = async (id: Todo["id"]) => {
+  const response = await fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE"
   });
   const data: Todo = await response.json();
 
