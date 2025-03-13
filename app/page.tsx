84a37f4ac618cc7583dc7ad966c21d27195087f6
Handsol <imhandsol@gmail.com>
2025-03-13 14:35:04 +0900

    feat: 페이지 및 컴포넌트 초기화

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
-import { getTodos } from "@/api/todo.api";
-import Link from "next/link";
-
 const HomePage = async () => {
-  const todos = await getTodos();
-
-  return (
-    <div>
-      HomePage
-      <Link href="/detail">To detail Page</Link>
-      <ul>
-        {todos.map(({ id, text, completed }) => (
-          <li key={id}>
-            {text} {completed ? "(Completed)" : "(Pending)"}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
