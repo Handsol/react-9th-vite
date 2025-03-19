54b41c1ad4d77ba1fc9ccdbb83419cf27e46f290
Handsol <imhandsol@gmail.com>
2025-03-19 14:57:08 +0900

    feat: TodoHeader 컴포넌트 추가 및 레이아웃에 통합

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TodoHeader from "@/components/todo/layout/TodoHeader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header>
-          <h1>Welcome to My App</h1>
-        </header>
+        <TodoHeader />
         {children}
       </body>
     </html>
---
+++ components/todo/layout/TodoHeader.tsx
@@ -0,0 +1,11 @@
+const TodoHeader = () => {
+  return (
+    <header>
+      <div className="container mx-auto p-2">
+        <h1 className="text-4xl font-bold">TodoList</h1>
+      </div>
+    </header>
+  );
+};
+
+export default TodoHeader;
