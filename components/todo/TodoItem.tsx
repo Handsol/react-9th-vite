0be122d67d51f047f3fa74d48ac6a2723d282e97
Handsol <imhandsol@gmail.com>
2025-03-24 09:27:56 +0900

    feat: TodoItem 컴포넌트에 조건부 클래스 추가

--- components/todo/TodoItem.tsx
+++ components/todo/TodoItem.tsx
@@ -4,6 +4,7 @@ import { deleteTodo, toggleTodoCompleted } from "@/api/todo.api";
 import { Todo } from "@/types/todo.type";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 interface TodoItemProps {
   todo: Todo;
@@ -14,7 +15,10 @@ const TodoItem = ({ todo }: TodoItemProps) => {
 
   return (
     <article className="flex flex-row items-center justify-between p-3 rounded-md border">
-      <Link href={`/${id}`} className="hover:underline">
+      <Link
+        href={`/${id}`}
+        className={cn("hover:underline", { "line-through": completed })}
+      >
         <h2>{text}</h2>
       </Link>
       <div className="space-x-2">
