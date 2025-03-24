47851a865b1dbf845e12feda61e30f5fb7e718c8
Handsol <imhandsol@gmail.com>
2025-03-24 09:33:24 +0900

    feat: TodoHeader에 링크 추가하여 홈으로 이동 가능하도록 개선

--- components/todo/layout/TodoHeader.tsx
+++ components/todo/layout/TodoHeader.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 const TodoHeader = () => {
   return (
     <header>
-      <div className="container mx-auto p-2">
-        <h1 className="text-4xl font-bold">TodoList</h1>
+      <div className="container flex flex-row mx-auto p-2">
+        <Link href="/">
+          <h1 className="text-4xl font-bold">TodoList</h1>
+        </Link>
       </div>
     </header>
   );
