addbfce12b6735b5f5e6404d110206223c31e0dc
Handsol <imhandsol@gmail.com>
2025-03-24 09:24:03 +0900

    style: TodoList 컴포넌트에 여백 추가

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { TodoList } from "@/components/todo/TodoList";
 const HomePage = async () => {
   return (
     <section>
-      <div className="container p-2 mx-auto">
+      <div className="container p-2 mx-auto space-y-4">
         <TodoForm />
         <TodoList />
       </div>
